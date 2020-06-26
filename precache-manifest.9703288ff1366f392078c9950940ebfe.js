59f7d556844dd3084494e8effe788f0c4cc3f837
Ryan <rjhold92@hotmail.co.nz>
2020-06-26 14:15:33 +1200

    Updates

---
+++ precache-manifest.9703288ff1366f392078c9950940ebfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c212e382dbca5553c82025710a6cae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9bc1bbc3148032b88d5",
+    "url": "/static/css/main.5dd24abc.chunk.css"
+  },
+  {
+    "revision": "6e4fc322443120c45e87",
+    "url": "/static/js/2.96950e0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96950e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9bc1bbc3148032b88d5",
+    "url": "/static/js/main.bed691f9.chunk.js"
+  },
+  {
+    "revision": "ad48f1be43dde9a59d6f",
+    "url": "/static/js/runtime-main.b350293a.js"
+  }
+]);
