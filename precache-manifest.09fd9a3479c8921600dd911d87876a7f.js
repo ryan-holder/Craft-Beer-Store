5cd8fecdd964637fbe16a79ceadfd49278747099
Ryan <rjhold92@hotmail.co.nz>
2020-06-26 13:54:09 +1200

    Updates

---
+++ precache-manifest.09fd9a3479c8921600dd911d87876a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd54d82b7c82aba9a155d42285f55a0a",
+    "url": "/Craft-Beer-Store/index.html"
+  },
+  {
+    "revision": "13bae348b7815e77e849",
+    "url": "/Craft-Beer-Store/static/css/main.5dd24abc.chunk.css"
+  },
+  {
+    "revision": "6e4fc322443120c45e87",
+    "url": "/Craft-Beer-Store/static/js/2.96950e0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Craft-Beer-Store/static/js/2.96950e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13bae348b7815e77e849",
+    "url": "/Craft-Beer-Store/static/js/main.7ffff721.chunk.js"
+  },
+  {
+    "revision": "db81724f8b1055374b89",
+    "url": "/Craft-Beer-Store/static/js/runtime-main.0bbf4ca2.js"
+  }
+]);
